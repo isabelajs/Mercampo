58dd6dc55561955161f603e7f5b6fe816b4c27cc
JuanC-JC <juancchaparro97@gmail.com>
2021-07-24 02:01:33 -0500

    add even  to move newProduct by click on buttonNewProduct - version mobile

--- src/componentes/ProfileProducts/ProfileProduct.jsx
+++ src/componentes/ProfileProducts/ProfileProduct.jsx
@@ -65,7 +65,7 @@ const ProfileButtonNewProduct = (props) => {
 
   return(
     <>
-      <div className="buttonNewProduct button button--icon button--main">
+      <div onClick={()=>{history.push('/profile/products/new')}} className="buttonNewProduct button button--icon button--main">
         <svg className='button__icon' width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
           <g clipPath="url(#clip0)">
           <path d="M22.7263 10.1483H14.8552V2.27722C14.8552 -0.757935 10.1482 -0.757935 10.1482 2.27722V10.1483H2.27712C-0.757251 10.1483 -0.757251 14.8553 2.27712 14.8553H10.1482V22.7264C10.1482 25.7616 14.8552 25.7616 14.8552 22.7264V14.8553H22.7263C25.7611 14.8553 25.7611 10.1483 22.7263 10.1483Z" fill="white"/>
