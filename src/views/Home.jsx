82c9d01399c8b5b95002e1e203c4f01713d0f98d
JuanC-JC <juancchaparro97@gmail.com>
2021-08-02 16:12:38 -0500

    Home responsive 320px

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -21,7 +21,7 @@ export default function Home(props) {
       <div className='home__info'>
         <h2 className='home__infoTitle'>Productos Campesinos <span> Frescos & asequibles </span></h2>
         <p className='home__infoText'>Apoya tu economía local descubriendo cientos de productos</p>
-        <button className='button button--main'>Productos del campo</button>
+        <button className='button button--main'>Nuestro Calendario</button>
       </div>
       <img loading='lazy' src={homeImg} alt="" />
     </div>
