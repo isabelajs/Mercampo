7566ef426144cf0d9b6bd390a6c074f3b77069cb
isabelajs <isabelajimenezs96@gmail.com>
2021-10-13 19:25:18 -0500

    add aboutUsSection component and styles, rename images from home

---
+++ src/componentes/Home/AboutUsSection.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import horizontalImg from '../../assets/static/Home/AboutUsSection/horizontalImage.png'
+import verticalImg from '../../assets/static/Home/AboutUsSection/verticalImage.png'
+
+import '../../assets/styles/componentes/Home/AboutUsSection.scss'
+export default function AboutUsSection () {
+    return (
+    <section className='aboutUsSection'>
+        <div className='aboutUsSection__info'>
+            <h2>¿Quiénes somos?</h2>
+            <p>Somos una plataforma digital que permite crear una relación directa entre productores agricolas con sus clientes potenciales, evitando intermediariarios en el proceso.</p>
+        </div>
+
+        <div className="aboutUsSection__helpsContainer">
+            <div className="aboutUsSection__help">
+                <img src="" alt="" />
+                <p>Agronomia Sostenible</p>
+            </div>
+            <div className="aboutUsSection__help">
+                <img src="" alt="" />
+                <p>Apoyo a la comunidad</p>
+            </div>
+            <div className="aboutUsSection__help">
+                <img src="" alt="" />
+                <p>Negocios Verdes</p>
+            </div>
+            <div className="aboutUsSection__help">
+                <img src="" alt="" />
+                <p>Emprendimiento</p>
+            </div>
+        </div>
+
+        <div className="aboutUsSection__imgsContainer">
+            <img src={horizontalImg} alt="" className="aboutUsSection__horizontalImg" />
+            <img src={verticalImg} alt="" className="aboutUsSection__verticalImg" />
+        </div>
+    </section>
+);
+};
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,56 +2,24 @@ import React from "react";
 import homeImg from '../assets/static/hero-1.jpg'
 import '../assets/styles/Home.scss'
 
-import imgQuienesSomosVertical from '../assets/static/imgHome-quienesSomos-vertical.png';
-import imgQuienesSomosHorizontal from '../assets/static/imgHome-quienesSomos-horizontal.png';
+import AboutUsSection from "../componentes/Home/AboutUsSection";
 
 export default function Home(props) {
 
   return (
-
     <main className="home">
       <div className='home-infoContainer'>
         <div className='home__info'>
-          <h2 className='home__infoTitle'>Productos Campesinos <span> Frescos & asequibles </span></h2>
+          <h1 className='home__infoTitle'>Productos Campesinos <span> Frescos & asequibles </span></h1>
           <p className='home__infoText'>Apoya tu economía local descubriendo cientos de productos</p>
           <button className='button button--main'>Nuestro Calendario</button>
         </div>
         <img loading='lazy' src={homeImg} alt="" />
       </div>
+      
 
+      <AboutUsSection/>
 
-      <section className="quienesSomos">
-
-        <div className='quienesSomos__info'>
-          <h2>¿Quiénes somos?</h2>
-          <p>Somos una plataforma digital que permite crear una relación directa entre productores agricolas con sus clientes potenciales, evitando intermediariarios en el proceso.</p>
-        </div>
-
-        <div className="quienesSomos__containerAyudas">
-          <div className="quienesSomos__ayuda">
-            <img src="" alt="" />
-            <p>Agronomia Sostenible</p>
-          </div>
-          <div className="quienesSomos__ayuda">
-            <img src="" alt="" />
-            <p>Apoyo a la comunidad</p>
-          </div>
-          <div className="quienesSomos__ayuda">
-            <img src="" alt="" />
-            <p>Negocios Verdes</p>
-          </div>
-          <div className="quienesSomos__ayuda">
-            <img src="" alt="" />
-            <p>Emprendimiento</p>
-          </div>
-        </div>
-
-        <div className="quienesSomos__containerImgs">
-          <img src={imgQuienesSomosHorizontal} alt="" className="quienesSomos__imgHorizontal" />
-          <img src={imgQuienesSomosVertical} alt="" className="quienesSomos__imgVertical" />
-        </div>
-
-      </section>
 
     </main>
   );
