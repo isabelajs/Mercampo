3e265e79e64631a94cf115bd619c064a15fe9a6a
isabelajs <isabelajimenezs96@gmail.com>
2021-10-15 15:00:52 -0500

    [ADD] OurGoalSection layout grid

--- src/componentes/Home/OurGoalSection/index.jsx
+++ src/componentes/Home/OurGoalSection/index.jsx
@@ -7,31 +7,28 @@ import '../../../assets/styles/componentes/Home/OurGoalSection/OurGoalSection.sc
 export default function OurGoalSection () {
     return (
     <section className='ourGoalSection'>
+
       <h2 className='green'>Nuestra Meta</h2>
 
-      <div className="ourGoalSection__content">
-
-        <div className="ourGoalSection__imgsContainer">
-          <img src={image02} alt="imagen acerca de nuestra meta" />
-          <img src={image01} alt="imagen acerca de nuestra meta" />
-        </div>
-
-        <div className="ourGoalSection__info">
-          <p className='ourGoalSection__text'>
-            Crear una plataforma en línea, con la 
-            participación de aliados estrategicos, donde los
-            diferentes productos agricolas de distintas partes
-            del país sean expuestos en una vitrina virtual, de 
-            manera que  los  productos sean más visibles, 
-            y la comunicación (entre vendedor--comprador)
-            sea directa, haciendo que  el proceso de venta sea
-            más rapido.
-          </p>
-
-          <button className="button button--second">Ver mas</button>
-        </div>
+      <div className="ourGoalSection__imgsContainer">
+        <img src={image02} alt="imagen acerca de nuestra meta" />
+        <img src={image01} alt="imagen acerca de nuestra meta" />
       </div>
 
+      <div className="ourGoalSection__info">
+        <p className='ourGoalSection__text'>
+          Crear una plataforma en línea, con la 
+          participación de aliados estrategicos, donde los
+          diferentes productos agricolas de distintas partes
+          del país sean expuestos en una vitrina virtual, de 
+          manera que  los  productos sean más visibles, 
+          y la comunicación (entre vendedor--comprador)
+          sea directa, haciendo que  el proceso de venta sea
+          más rapido.
+        </p>
+
+        <button className="button button--second">Ver mas</button>
+      </div>
 
     </section>
 );
