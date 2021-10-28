7cf617868fca322bfeb582cad5c11d61672d1abd
isabelaJs <isabelajimenezs96@gmail.com>
2021-10-28 13:45:36 -0500

    [MOD] change icons aboutUs section

--- src/componentes/Home/AboutUsSection/index.jsx
+++ src/componentes/Home/AboutUsSection/index.jsx
@@ -1,54 +1,54 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 
 //assets
 import horizontalImg from '@images/Home/AboutUsSection/horizontalImage.png'
 import verticalImg from '@images/Home/AboutUsSection/verticalImage.png'
-import sostenibilityImg from '@images/Home/AboutUsSection/plant.png'
-import {useScrollReveal} from '@hooks'
+import sostenibilityImg from '@images/Home/AboutUsSection/plant.svg'
+import { useScrollReveal } from '@hooks'
 
 //styles
 import '@styles/componentes/Home/AboutUsSection.scss'
 
-export default function AboutUsSection () {
+export default function AboutUsSection() {
 
     const refSection = useRef(null)
 
-    useScrollReveal([refSection],{
-        root:null,
-        threshold:0.25,
+    useScrollReveal([refSection], {
+        root: null,
+        threshold: 0.25,
         rootMargin: "350px"
     })
 
     return (
-    <section ref={refSection} className='aboutUsSection section--gray  section--separation fade-down fade-in--right'>
-        <div className='aboutUsSection__info'>
-            <h2>¿Quiénes somos?</h2>
-            <p>Somos una plataforma digital que permite crear una relación directa entre productores agricolas con sus clientes potenciales, evitando intermediariarios en el proceso.</p>
-        </div>
+        <section ref={refSection} className='aboutUsSection section--gray  section--separation fade-down fade-in--right'>
+            <div className='aboutUsSection__info'>
+                <h2>¿Quiénes somos?</h2>
+                <p>Somos una plataforma digital que permite crear una relación directa entre productores agricolas con sus clientes potenciales, evitando intermediariarios en el proceso.</p>
+            </div>
 
-        <div className="aboutUsSection__helpsContainer">
-            <div className="aboutUsSection__help">
-                <img src={sostenibilityImg} alt="" />
-                <h4>Agronomia Sostenible</h4>
+            <div className="aboutUsSection__helpsContainer">
+                <div className="aboutUsSection__help">
+                    <img src={sostenibilityImg} alt="" />
+                    <h4>Agronomia Sostenible</h4>
+                </div>
+                <div className="aboutUsSection__help">
+                    <img src={sostenibilityImg} alt="" />
+                    <h4>Apoyo a la comunidad</h4>
+                </div>
+                <div className="aboutUsSection__help">
+                    <img src={sostenibilityImg} alt="" />
+                    <h4>Negocios Verdes</h4>
+                </div>
+                <div className="aboutUsSection__help">
+                    <img src={sostenibilityImg} alt="" />
+                    <h4>Emprendimiento</h4>
+                </div>
             </div>
-            <div className="aboutUsSection__help">
-                <img src={sostenibilityImg} alt="" />
-                <h4>Apoyo a la comunidad</h4>
-            </div>
-            <div className="aboutUsSection__help">
-                <img src={sostenibilityImg} alt="" />
-                <h4>Negocios Verdes</h4>
-            </div>
-            <div className="aboutUsSection__help">
-                <img src={sostenibilityImg} alt="" />
-                <h4>Emprendimiento</h4>
-            </div>
-        </div>
 
-        <div className="aboutUsSection__imgsContainer">
-            <img src={horizontalImg} alt="" className="aboutUsSection__horizontalImg" />
-            <img src={verticalImg} alt="" className="aboutUsSection__verticalImg" />
-        </div>
-    </section>
-);
+            <div className="aboutUsSection__imgsContainer">
+                <img src={horizontalImg} alt="" className="aboutUsSection__horizontalImg" />
+                <img src={verticalImg} alt="" className="aboutUsSection__verticalImg" />
+            </div>
+        </section>
+    );
 };
--- src/componentes/Home/index.js
+++ src/componentes/Home/index.js
@@ -1,6 +1,25 @@
-export {default as AboutUsSection} from './AboutUsSection'; 
-export {default as HowItWorksSection} from './HowItWorksSection'; 
-export {default as ImpactSection} from './ImpactSection'; 
-export {default as IntroSection} from './IntroSection'; 
-export {default as NewsSection} from './NewsSection'; 
-export {default as OurGoalSection} from './OurGoalSection'; 
+export { default as AboutUsSection } from "./AboutUsSection";
+export { default as HowItWorksSection } from "./HowItWorksSection";
+export { default as ImpactSection } from "./ImpactSection";
+export { default as IntroSection } from "./IntroSection";
+export { default as NewsSection } from "./NewsSection";
+export { default as OurGoalSection } from "./OurGoalSection";
+
+const list = [2, 4, 0, 1];
+
+const solution = (lista) => {
+  const newList = lista.map(() => 0);
+
+  let counter = 0;
+
+  list.forEach((element) => {
+    if (element !== 0) {
+      newList[counter] = element;
+      counter++;
+    }
+  });
+
+  return newList;
+};
+
+console.log(solution(list));
